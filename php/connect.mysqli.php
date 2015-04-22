de4da4824ed077bfa395c823ecc1e8e4e4ee4139
cholalo <violinist.ch@gmail.com>
2015-04-22 10:04:28 +0900

    ...

--- php/connect.mysqli.php
+++ php/connect.mysqli.php
@@ -1,9 +1,9 @@
 <?php
 
 define("DB_HOST","localhost");
-define("DB_USER","cholalo");
-define("DB_PASS","aquamarineBellatrix!");
-define("DB_NAME","cholalo_db_cholalo");
+define("DB_USER","____");
+define("DB_PASS","____");
+define("DB_NAME","____");
 
 $con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die(mysqli_errno());
 
